af3ab41884b26ecaae9a872ac981f6fd1ba401a0
LarynaB <lbillingh@gmail.com>
2024-07-13 13:22:31 -0700

    Update FollowPlayer.cs

--- FollowPlayer.cs
+++ FollowPlayer.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {   //public access to gameobject "player"
     public GameObject player;
+    //vector on top of vector for correct positioning
+    private Vector3 offset = new Vector3(0,5,-7);
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +17,6 @@ public class FollowPlayer : MonoBehaviour
     void Update()
     {
         //follow position of player
-       transform.position = player.transform.position + new Vector3(0,5,-7); 
+       transform.position = player.transform.position + offset; 
     }
 }
