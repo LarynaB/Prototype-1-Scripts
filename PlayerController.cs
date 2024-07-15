847cf532855d9307b85133da3d675dae2dbef3c5
LarynaB <lbillingh@gmail.com>
2024-07-15 11:07:16 -0700

    horizontal input using get axis for string horizontal

--- PlayerController.cs
+++ PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
     //user access set to public to view/edit "speed" in Unity
     public float speed = 5.0f;
     public float turnSpeed;
+    public float horizontalInput;
 
     // Start is called before the first frame update
     void Start()
@@ -16,9 +17,10 @@ public class PlayerController : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
+    {   //Get Axis method is asking what axis to use in the form of a string
+        horizontalInput = Input.GetAxis("Horizontal");
         //move the vehicle forward
         transform.Translate(Vector3.forward * Time.deltaTime * speed);
-        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed);
+        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
