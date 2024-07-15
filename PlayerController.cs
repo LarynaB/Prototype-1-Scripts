91129c1250b12c2d89ab2c7e85d199cf5815b121
LarynaB <lbillingh@gmail.com>
2024-07-14 17:04:18 -0700

    turnSpeed using vector3 right

--- PlayerController.cs
+++ PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     //user access set to public to view/edit "speed" in Unity
     public float speed = 5.0f;
+    public float turnSpeed;
 
     // Start is called before the first frame update
     void Start()
@@ -18,5 +19,6 @@ public class PlayerController : MonoBehaviour
     {
         //move the vehicle forward
         transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed);
     }
 }
